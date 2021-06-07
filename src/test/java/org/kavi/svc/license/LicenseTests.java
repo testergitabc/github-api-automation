938e8a1478f5b7ced30eeeaf8a2ffecb93cb9f2d
testergitabc <85275599+testergitabc@users.noreply.github.com>
2021-06-06 22:57:26 -0500

    extending from BaseTest

--- src/test/java/org/kavi/svc/license/LicenseTests.java
+++ src/test/java/org/kavi/svc/license/LicenseTests.java
@@ -1,26 +1,23 @@
 package org.kavi.svc.license;
 
-import io.restassured.response.Response;
-import org.testng.Assert;
 import org.testng.annotations.Test;
+import utils.BaseTest;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.notNullValue;
 
-public class LicenseTests {
-    private static final String BASE_URL = "https://api.github.com";
+public class LicenseTests extends BaseTest {
 
     @Test
     public void sendAListRequestAndValidateResponse() {
         given()
                 .when()
-                //.auth().basic("", "") basic authentication - Not required for this End Point
-                .get(BASE_URL + "/licenses")
+                .auth().basic("", "")//input username/password
+                .get(getBaseUrl() + "/licenses")
                 .then()
                 .statusCode(200)
-                .body("[0].name", notNullValue())  // Make sure it is not null
-                .body("[0].key", equalTo("agpl-3.0"));  // Verify that agpl-3.0 is supported
-//        Assert.assertEquals(response.getStatusCode(), 200);
+                .body("[0].name", notNullValue())
+                .body("[0].key", equalTo("agpl-3.0"));
     }
 }
